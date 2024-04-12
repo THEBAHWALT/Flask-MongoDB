4d0e919996bf29b8c9aa2076ed6e5858f0c14f41
walter <walterlin@ma-tek.com>
2024-04-12 09:49:56 +0800

    Static FIles in Flask

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
-app = Flask(__name__)
+# 建立 Application 物件，設定靜態檔案路徑處理
+app = Flask(
+    __name__,
+    static_folder="public", # 靜態檔案資料夾名稱
+    # static_url_path = "/abc" # 127.0.0.1:82/abc/test.txt
+    static_url_path="/" # 對應路徑 # 127.0.0.1:82/abc/test.txt
+)
+# 所有在 static 資料夾底下的檔案，都對應到網址路徑 / 名稱
 
 # 建立路徑 / 對應的處理函式
 @app.route("/")
 def index(): # 用來回應路徑 / 處理函式
     return "Test" # 回傳網站首頁的內容
+
 # 建立路徑 /data 對應的處理函式
 @app.route("/data")
 def handleData():
