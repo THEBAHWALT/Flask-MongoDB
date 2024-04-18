272e26109ca895c44e572dafd3d3be4bd0768d39
walter <walterlin@ma-tek.com>
2024-04-18 18:56:01 +0800

    Hypertext Reference and Image

--- template.py
+++ template.py
@@ -1,14 +1,20 @@
+import os
+print("Current working directory:", os.getcwd())
 from flask import Flask
 from flask import render_template
-app = Flask(
-    __name__,
-    static_folder="public",
-    static_url_path="/"
-)
+app = Flask(__name__,
+            template_folder='D:/Python/Flask/templates', 
+            static_folder="public", 
+            static_url_path="/")
+
 # 建立路徑 / 對應的處理函式
 @app.route("/")
 def index():
-    return render_template("index", name = "Walter")
+    return render_template("index.html")
+# 處理路徑 / 的對應函式
+@app.route("/page")
+def page():
+    return render_template("page.html")
 
 # 啟動伺服器
 app.run(port = 82)
